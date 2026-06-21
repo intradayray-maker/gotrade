011261a73ad411fa160cb87ed74b4f0eccf0e4e3
intradayray-maker <intradayray@gmail.com>
2026-06-21 05:46:19 -0400

    supa

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-// utils\supabase\server.ts
+// utils\supabase\server.ts (test)
 
-=======
-//utils\supabase\server.ts
-export const runtime = "nodejs";
->>>>>>> f8b5146b38fa55ecb4b35d4d62828ee629543d0f
 
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
@@ -21,7 +16,6 @@ export async function createSupabaseServerClient() {
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: any) {
-<<<<<<< HEAD
           try {
             cookieStore.set({ name, value, ...options });
           } catch {
@@ -34,18 +28,8 @@ export async function createSupabaseServerClient() {
           } catch {
             // ignore
           }
-=======
-          cookieStore.set(name, value, options);
-        },
-        remove(name: string, options: any) {
-          cookieStore.set(name, "", options);
->>>>>>> f8b5146b38fa55ecb4b35d4d62828ee629543d0f
         },
       },
     }
   );
 }
-<<<<<<< HEAD
-=======
-// tezt2
->>>>>>> f8b5146b38fa55ecb4b35d4d62828ee629543d0f
